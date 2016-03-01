2396ce83ea3604dd6bd931a342e1e3e391b202f6
Nicholas Crumrine <uncletammy@yahoo.com>
2016-02-29 20:48:17 -0600

    I need access

--- api/models/user.js
+++ api/models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcryptjs');
+
+
 module.exports = {
   attributes: {
     email: {
@@ -6,5 +9,22 @@ module.exports = {
     username: {
     	unique: true
     }
-  }
+  },
+
+  beforeUpdate: function(vals,done){
+    // If a password is passed in, hash it before continuing
+    if (vals.password) {
+      bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(vals.password, salt, function(err, hash) {
+            if (err) return err;
+
+            vals.password = hash;
+            return done();
+          });
+      });
+    } else {
+      return done();
+    }
+  },
+
 };
